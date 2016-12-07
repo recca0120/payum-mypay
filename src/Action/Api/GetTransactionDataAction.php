73d9de4f435eb047c8a365954cc147cb97ac6c06
Recca Tsai <recca0120.gmail.com>
2016-12-07 19:52:35 +0800

    don't replace model when verify hash is fail

--- src/Action/Api/GetTransactionDataAction.php
+++ src/Action/Api/GetTransactionDataAction.php
@@ -19,7 +19,13 @@ class GetTransactionDataAction extends BaseApiAwareAction
 
         $details = ArrayObject::ensureArrayObject($request->getModel());
 
-        $details->replace($this->api->getTransactionData((array) $details));
+        $result = $this->api->getTransactionData((array) $details);
+
+        if (isset($result['code']) === true && $result['code'] === '-1') {
+            return;
+        }
+
+        $details->replace($result);
     }
 
     /**
